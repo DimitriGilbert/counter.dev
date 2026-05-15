2330cd0239772d0b3d810e311ffc4ae121798096
DimitriGilbert <dimitri.gilbert@gmail.com>
2026-05-15 20:45:17 +0200

    DNT browsers still trigger view counting

--- docs/script.js
+++ docs/script.js
@@ -1,31 +1,24 @@
 (function () {
-    if (sessionStorage.getItem("doNotTrack") || localStorage.getItem("doNotTrack")) {
-        return;
-    }
     var id = document.currentScript.getAttribute("data-id");
     var utcoffset = document.currentScript.getAttribute("data-utcoffset");
     var server = document.currentScript.getAttribute("data-server") || "https://t.counter.dev";
+    var dnt = sessionStorage.getItem("doNotTrack") || localStorage.getItem("doNotTrack");
 
     if (!sessionStorage.getItem("_swa") && !document.referrer.startsWith(location.protocol + "//" + location.host)) {
         setTimeout(function () {
             sessionStorage.setItem("_swa", "1");
-            fetch(
-                server +
-                    "/track?" +
-                    new URLSearchParams({
-                        referrer: document.referrer,
-                        screen: screen.width + "x" + screen.height,
-                        id: id,
-                        utcoffset: utcoffset,
-                    }),
-            );
+            var params = { id: id, utcoffset: utcoffset };
+            if (!dnt) {
+                params.referrer = document.referrer;
+                params.screen = screen.width + "x" + screen.height;
+            }
+            fetch(server + "/track?" + new URLSearchParams(params));
         }, 4500);
     }
-    navigator.sendBeacon(
-        server + "/trackpage",
-        new URLSearchParams({
-            id: id,
-            page: window.location.pathname,
-        }),
-    );
+    if (!dnt) {
+        navigator.sendBeacon(
+            server + "/trackpage",
+            new URLSearchParams({ id: id, page: window.location.pathname }),
+        );
+    }
 })();
